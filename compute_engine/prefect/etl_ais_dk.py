722c00af96e4f3fb6a3b7b44fdfeb5b4e7b8796e
larsskaret <lars.skaret@gmail.com>
2023-04-04 18:41:58 +0200

    add prefect premade

---
+++ compute_engine/prefect/etl_ais_dk.py
@@ -0,0 +1,144 @@
+import os
+import argparse
+import pandas as pd
+
+from tqdm.auto import tqdm
+from datetime import date, timedelta
+from pathlib import Path
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp import GcpCredentials
+from prefect_dbt import DbtCoreOperation
+
+from global_vars import BLOCK_GCS_BUCKET, COLUMN_NAMES_DK, GCS_BUCKET_LOC_DK, SCHEMA_DK, \
+BLOCK_GCP_CRED, GCP_TABLE_DK, GCP_PROJECT_ID,  BQ_SCHEMA_DK
+
+@task(retries=3)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read ais data from web into pandas DataFrame."""
+    print(f"Url to download {dataset_url}")
+    df = pd.read_csv(dataset_url, na_values="Unknown")
+    return df
+
+
+@task(log_prints=True)
+def clean_col_dtype(df=pd.DataFrame) -> pd.DataFrame:
+    """Clean column names and dtype."""
+
+    df.columns = COLUMN_NAMES_DK
+    df = df.astype(SCHEMA_DK)
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task(log_prints=True)
+def write_local(df: pd.DataFrame) -> Path:
+    """Write DataFrame out locally as parquet file."""
+    path = Path("temp.parquet")
+    df.to_parquet(path, compression="snappy")
+    return path
+
+@task()
+def write_gcs(loc_path: Path, gcs_path: Path) -> None:
+    """Upload local parquet file to GCS."""
+
+    gcs_block = GcsBucket.load(BLOCK_GCS_BUCKET)
+    gcs_block.upload_from_path(from_path=loc_path, to_path=gcs_path)
+    return
+
+@task(log_prints=True)
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BiqQuery."""
+
+    gcp_credentials_block = GcpCredentials.load(BLOCK_GCP_CRED)
+    print(f"BigQuery destination table: {GCP_TABLE_DK}")
+    df.to_gbq(
+        destination_table=GCP_TABLE_DK,
+        project_id=GCP_PROJECT_ID,
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+        table_schema=BQ_SCHEMA_DK,
+    )
+    print(f"Wrote {len(df)} lines to table {GCP_TABLE_DK} for GCP prject {GCP_PROJECT_ID}.")
+
+@task
+def dbt_transform() -> None:
+    """Run the dbt transformations."""
+
+    dbt_path = f"../../{os.getenv('DBT_PROJECT_PATH')}"
+
+    dbt_op = DbtCoreOperation(
+        commands=["dbt build --var 'is_test_run: false' --target 'prod'"],
+        working_dir=dbt_path,
+        project_dir=dbt_path,
+        profiles_dir=dbt_path,
+    )
+
+    dbt_op.run()
+
+@flow(log_prints=True)
+def etl_ais_dk(year: int = 0, month: int = 0, day: int = 0) -> None:
+    """Extract data from web and place it in GCS and BigQuery.
+    Oldest data in day format is 2022.11.01
+    Newest data is 3 or 4 days old, depending on time zone.
+    Date will not be checked."""  
+    tqdm.pandas()
+    #Using silly workaround. Should do a proper check. Perhaps use date format.
+    if year != 0 and month != 0 and day != 0:
+        dataset_file = f"aisdk-{year}-{month:02}-{day:02}"
+        
+        dataset_url = f"https://web.ais.dk/aisdata/{dataset_file}.zip"
+        gcs_path = f"{GCS_BUCKET_LOC_DK}/{year}/{month}/{dataset_file}.parquet"
+
+        df = fetch(dataset_url)
+        df_clean = clean_col_dtype(df)
+        path = write_local(df_clean)
+        write_gcs(path, gcs_path)
+        write_bq(df)
+        os.system(f"rm {path}")
+        dbt_transform()
+
+    else:
+        print(f"Improper input y/m/d: {year}/{month}/{day}.")
+
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+@flow(log_prints=True)
+def etl_ais_dk_date(start_date: date = date(1,1,1), end_date: date = date(1,1,1)) -> None:
+    """Specify start_date and end_date, will be checked."""
+
+    if (start_date >= date(2022,11,1) and
+        end_date <= (date.today() - timedelta(4)) and
+        start_date < end_date):
+
+        for dl_date in daterange(start_date, end_date):
+            etl_ais_dk(dl_date.year, dl_date.month, dl_date.day)
+    else:
+        print("Day format data: Earliest date is 2022.11.01 and latest date is 4 days before today.")
+
+
+
+@flow(log_prints=True)
+def etl_ais_dk_std(dl_date: date = date(1,1,1)) -> None:
+    """Extract data form 4 days ago."""
+    if(dl_date == date(1,1,1)):
+        dl_date = date.today() - timedelta(4)
+
+    if dl_date < date(2022,11,1) or dl_date > (date.today() - timedelta(4)):
+        print("Day format data: Earliest date is 2022.11.01 and latest date is 4 days before today.")
+    else:
+        etl_ais_dk(dl_date.year, dl_date.month, dl_date.day) 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--dl_date', required=False, default=date(1,1,1), help='Date to download, default: 4 days before today.')
+    args = parser.parse_args()
+    
+    etl_ais_dk_std(args.dl_date)
--- compute_engine/prefect/global_vars.py
+++ compute_engine/prefect/global_vars.py
@@ -1,7 +1,7 @@
 import os
 
-GCS_TABLE_DK = os.getenv("GCP_DATALAKE_PATH_DK")+'.'+os.getenv("GCP_BQ_TABLE_DK")#In bigquery: dateset.table
-GCS_PROJECT_ID = os.getenv("GCS_PROJECT_ID")
+GCP_TABLE_DK = os.getenv("GCP_BQ_DATASET")+'.'+os.getenv("GCP_BQ_TABLE_DK")#In bigquery: dataset.table
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 BLOCK_GCP_CRED = os.getenv("PREFECT_GCP_CRED_BLOCK")
 BLOCK_GCS_BUCKET = os.getenv("PREFECT_GCP_BUCKET_BLOCK") 
 GCS_BUCKET_LOC_DK = os.getenv("GCP_DATALAKE_PATH_DK")
@@ -10,7 +10,7 @@ GCS_BUCKET_LOC_DK = os.getenv("GCP_DATALAKE_PATH_DK")
 SCHEMA_DK = {
     'timestamp'     : 'datetime64[ns]',
     'source_type'   : 'string',
-    'mmsi'          : 'Int64',
+    'mmsi'          : 'string',
     'lat'           : 'float64',
     'lon'           : 'float64',
     'nav_status'    : 'string',
@@ -36,6 +36,34 @@ SCHEMA_DK = {
     'd'             : 'float64'
     }
 
+BQ_SCHEMA_DK = [
+    {'name':'timestamp',     'type': 'datetime'},
+    {'name':'source_type',   'type': 'string'},
+    {'name':'mmsi',          'type': 'string'},
+    {'name':'lat',           'type': 'float64'},
+    {'name':'lon',           'type': 'float64'},
+    {'name':'nav_status',    'type': 'string'},
+    {'name':'rot',           'type': 'float64'},
+    {'name':'sog',           'type': 'float64'},
+    {'name':'cog',           'type': 'float64'},
+    {'name':'heading',       'type': 'float64'},
+    {'name':'imo',           'type': 'int64'},
+    {'name':'callsign',      'type': 'string'},
+    {'name':'name',          'type': 'string'},
+    {'name':'ship_type',     'type': 'string'},
+    {'name':'cargo_type',    'type': 'string'},
+    {'name':'width',         'type': 'float64'},
+    {'name':'length',        'type': 'float64'},
+    {'name':'pfd_type',      'type': 'string'},
+    {'name':'draught',       'type': 'float64'},
+    {'name':'destination',   'type': 'string'},
+    {'name':'eta',           'type': 'datetime'},
+    {'name':'data_source',   'type': 'string'},
+    {'name':'a',             'type': 'float64'},
+    {'name':'b',             'type': 'float64'},
+    {'name':'c',             'type': 'float64'},
+    {'name':'d',             'type': 'float64'}
+    ]
 #Connect this to terraform somehow?
 COLUMN_NAMES_DK = [
     'timestamp',
