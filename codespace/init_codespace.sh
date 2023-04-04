9b9ec1be035bc4670b6ba5fe289e0050802b70dc
larsskaret <99684354+larsskaret@users.noreply.github.com>
2023-04-04 22:16:24 +0000

    sh changes

--- codespace/init_codespace.sh
+++ codespace/init_codespace.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -o allexport && source .env && set +o allexport
+mkdir ../compute_engine/.secrets
+mkdir .secrets
+
 
 echo -e "\n"
 echo -e "#####################################"
@@ -15,52 +18,24 @@ sudo apt-get update && sudo apt-get install google-cloud-cli
 
 #Intialize or login gcloud
 cat << EOF
+
 ########################################
 #                                      #
 # Let's log in to gcloud               #
 # PS. Paste in codespeace terminal is  #
 # both cmd/ctrl+alt+v and cmd/ctrl+v   #
 #                                      #
-########################################\n
+########################################
+
 EOF
 
-gcloud auth login
+gcloud auth application-default login
+#gcloud auth login
 
 GCP_PROJECT_ID=${GCP_PROJECT_NAME}-${RANDOM}
-       
-while : ; do
-    echo -e "[1] Create a new project (ID: ${GCP_PROJECT_ID})."
-    echo -e "[2] Use an existing project. Will take you to GCP interactive mode (gcloud init)."
-    echo -e "[q] Quit\n"
-    read -r INPUT
-
-    if [[ "${INPUT}" == "q" ]]
-    then
-        echo -e "Exit\n"
-        break
-    elif [[ "${INPUT}" == "1" ]]
-    then
-        echo -e "Project id: ${GCP_PROJECT_ID}"
-        echo -e "Project name: ${GCP_PROJECT_NAME}\n"
-        gcloud projects create "${GCP_PROJECT_ID}" --name="${GCP_PROJECT_NAME}" --verbosity=none --set-as-default
-        if [ $? -eq 0 ] 
-        then
-            echo -e "Successfully created project\n" 
-            break
-        else 
-            echo -e "Unsuccessfull\n"
-        fi
-    elif [[ "${INPUT}" == "2" ]]
-    then
-        gcloud init
-        GCP_PROJECT_ID=$(gcloud config get project)
-        echo -e "Current project is ${GCP_PROJECT_ID}"
-    else
-        echo -e "Try again.\n"
-    fi
-done
-
-echo -e "Writing GCP_PROJECT_ID, GCP_PROJECT_NR and BILLING_ID to .env\n"
+ 
+echo -e "Writing GCP_PROJECT_ID and BILLING_ID to .env\n"
+echo -e "Assumes there is only one billing account!"
 sleep 2
 
 line=$(grep -n "GCP_PROJECT_ID=" .env | cut -d: -f1)
@@ -70,12 +45,19 @@ BILLING_ID=$(gcloud beta billing accounts list | grep -Eo '.{6}-.{6}-.{6}')
 line=$(grep -n "GCP_BILLING_ID=" .env | cut -d: -f1)
 sed -i "${line}s/$/${BILLING_ID}/" .env
 
-PROJECT_NR=$(gcloud projects list \
---filter="$(gcloud config get-value project)" \
---format="value(PROJECT_NUMBER)")
-line=$(grep -n "GCP_PROJECT_NR=" .env | cut -d: -f1)
-sed -i "${line}s/$/${PROJECT_NR}/" .env
+#PROJECT_NR=$(gcloud projects list \
+#--filter="$(gcloud config get-value project)" \
+#--format="value(PROJECT_NUMBER)")
+#line=$(grep -n "GCP_PROJECT_NR=" .env | cut -d: -f1)
+#sed -i "${line}s/$/${PROJECT_NR}/" .env
 
+mkdir .ssh
+ssh-keygen -t rsa -f ./${GCP_SSH_KEY_LOCATION} -C ${GCP_COMPUTE_USERNAME} -b 2048
+
+chmod 600 ./${GCP_SSH_KEY_LOCATION}
+chmod 600 ./${GCP_SSH_KEY_LOCATION}.pub
+
+set -o allexport && source .env && set +o allexport
 
 echo Install Terraform
 sleep 2
@@ -83,18 +65,39 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
-echo -e "Terraform module for setting up GCP project,"
-echo -e "service accounts, credential keys and APIs.\n"
+
+
+cat << EOF
+
+################################################
+#                                              #
+# Terraform module for setting up GCP project, #
+# service accounts, credential keys and APIs.  #
+#                                              #
+################################################
+
+EOF
 sleep 2
 
 cd terraform_gcp_project
 terraform init
-terraform apply -var="project=${GCP_PROJECT_ID}" #-auto-approve
+terraform apply #-auto-approve
 
-echo -e "Terraform module for setting up GCP project resources"
-echo -e "such as compute instance, BigQuery and GCS.\n"
+cat << EOF
+
+#########################################################
+#                                                       #
+# Terraform module for setting up GCP project resources #
+# such as compute instance, BigQuery and GCS.           #
+#                                                       #
+#########################################################
+
+EOF
 sleep 2
 
 cd ../terraform_gcp_resources
 terraform init
-terraform apply -var="project=${GCP_PROJECT_ID}" #-auto-approve
+terraform apply #-auto-approve
+
+cd ..
+cp .env ../compute_engine/.env
--- compute_engine/init_compute_engine.sh
+++ compute_engine/init_compute_engine.sh
@@ -6,38 +6,38 @@ sudo touch /var/lib/man-db/auto-update
 sudo apt update
 sudo apt -y upgrade
 
-cd ais_pipeline
+cd compute_engine
 #.env location should come from env variable
-set -o allexport && source .secrets/.env && set +o allexport
-echo "Here: 1"
+set -o allexport && source .env && set +o allexport
+
 sudo apt install python3.8-venv -y
 python3 -m venv ais-env
-source ais-env/bin/activate
+
 
 #Is pip installed?
 sudo apt install pip -y
 pip install -r requirements.txt
 
 #Prefect
-prefect cloud login -k $"PREFECT_KEY"
+prefect cloud login -k $PREFECT_KEY
 
 #Create blocks
-python3 create_gcp_blocks.py
+python3 prefect/create_gcp_blocks.py
 
 #prefect deployment - should connect these to github
-prefect deployment build etl_ais_dk.py:etl_ais_dk_std \
+prefect deployment build prefect/etl_ais_dk.py:etl_ais_dk_std \
 --name "etl_ais_dk" \
---work-queue $"PREFECT_QUEUE" \
+--work-queue $PREFECT_QUEUE \
 --cron "0 6 * * *" \
+--timezone 'Europe/Paris' \
 --apply
 
 #To download data for multiple consecutive days.
-prefect deployment build etl_ais_dk.py:etl_ais_dk_date \
+prefect deployment build prefect/etl_ais_dk.py:etl_ais_dk_date \
 --name "etl_ais_dk_date" \
---work-queue $"PREFECT_QUEUE" \
---description "Call this from prefect cloud to download data for several days. Earliest 2022 11 01, latest 4 days ago." \
+--work-queue $PREFECT_QUEUE \
 --apply
 
 #prefect agent
-tmux new-session -d -s pf_session 'prefect agent start -q $"PREFECT_QUEUE"'
+tmux new-session -d -s pf_session 'prefect agent start -q $PREFECT_QUEUE'
 
