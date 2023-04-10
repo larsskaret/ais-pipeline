05df8c91260b044ba09273ee801970eb38a6e625
larsskaret <lars.skaret@gmail.com>
2023-04-10 17:26:09 +0200

    Prettify prints

--- codespace/init_codespace.sh
+++ codespace/init_codespace.sh
@@ -50,9 +50,17 @@ EOF
 gcloud auth login
 
 GCP_PROJECT_ID=${GCP_PROJECT_NAME}-${RANDOM}
- 
-echo -e "Writing GCP_PROJECT_ID and BILLING_ID to .env\n"
-echo -e "Assumes there is only one billing account!"
+
+cat << EOF
+
+#################################################
+#                                               #
+# Writing GCP_PROJECT_ID and BILLING_ID to .env #
+# Assumes there is only one billing account!    #
+#                                               #
+#################################################
+
+EOF
 sleep 2
 
 line=$(grep -n "GCP_PROJECT_ID=" .env | cut -d: -f1)
@@ -86,8 +94,17 @@ chmod 600 ./${GCP_SSH_KEY_LOCATION}.pub
 
 set -o allexport && source .env && set +o allexport
 
-echo Install Terraform
+cat << EOF
+
+#####################
+#                   #
+# Install Terraform #
+#                   #
+#####################
+
+EOF
 sleep 2
+
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
